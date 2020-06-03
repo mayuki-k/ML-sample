e63508d721e42e8975ff48e952481cd07c80e888
mayuki-k <ymtech0615@gmail.com>
2020-06-03 23:41:34 +0900

    logistic整理

--- logistic.py
+++ logistic.py
@@ -5,13 +5,13 @@ from sklearn.model_selection import train_test_split
 # データ取得モジュール
 from env import make1
 
-label, data = make1.get_data()
+l, d = make1.get_data()
 
-def logistic():
+def logistic(data, label):
     x_train, x_test, y_train, y_test = train_test_split(data, label, random_state=0)
     pre = linear_model.LogisticRegression(random_state=0)
     pre.fit(x_train, y_train)
     print(f'score = {pre.score(x_test, y_test)}')
     print(pre.predict([[22, 8]]))
 
-logistic()
+logistic(d, l)
