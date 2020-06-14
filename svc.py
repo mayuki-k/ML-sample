53b5815679e9193c049929eedca45df354e64789
mayuki-k <ymtech0615@gmail.com>
2020-06-14 22:42:53 +0900

    ソフトマージンの処理の追加

--- svc.py
+++ svc.py
@@ -3,7 +3,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import svm
 
 # データの取得
-from env import make2
+from env import make2, make3
 
 x, y = make2.get_datas()
 
@@ -14,6 +14,12 @@ def standalization(data):
 
 def support_vector_classification(x, y):
     f = svm.LinearSVC(random_state=0)
+    f.fit(x, y)
+    print(f.predict([[0, 1]]))
+
+def support_vector_classification_soft(x, y):
+    #Cはマージン最大化とペナルティ最小化のバランスの定数。デフォルトは0.2
+    f = svm.LinearSVC(C=0.2, random_state=0)
     f.fit(x, y)
     print(f.predict([[0, 1]]))
 
